b1dcc96a0570dc4c8e79289eb8d5223773e7e253
j10sanders <jps458@nyu.edu>
2015-12-06 15:12:35 +0000

    logout functionality + error handling for page limit

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from flask import render_template
 from blog import app
 from .database import session, Entry
 from flask import flash
-from flask.ext.login import login_user
+from flask.ext.login import login_user, logout_user
 from werkzeug.security import check_password_hash
 from .database import User
 from flask import request, redirect, url_for
@@ -15,29 +15,62 @@ PAGINATE_BY = 10
 @app.route("/")
 @app.route("/page/<int:page>")
 def entries(page=1):
-    # Zero-indexed page
-    page_index = page - 1
-
-    count = session.query(Entry).count()
-
-    start = page_index * PAGINATE_BY
-    end = start + PAGINATE_BY
-
-    total_pages = (count - 1) / PAGINATE_BY + 1
-    has_next = page_index < total_pages - 1
-    has_prev = page_index > 0
-
-    entries = session.query(Entry)
-    entries = entries.order_by(Entry.datetime.desc())
-    entries = entries[start:end]
-
-    return render_template("entries.html",
-        entries=entries,
-        has_next=has_next,
-        has_prev=has_prev,
-        page=page,
-        total_pages=total_pages
-    )
+    print(request.args)
+    try:
+        limit = int(request.args.get("limit", PAGINATE_BY))
+    
+        #try and if get value error, set limit to paginate_by
+        
+        # Zero-indexed page
+        page_index = page - 1
+    
+        count = session.query(Entry).count()
+    
+        start = page_index * limit
+        end = start + limit
+    
+        total_pages = (count - 1) / limit + 1
+        has_next = page_index < total_pages - 1
+        has_prev = page_index > 0
+    
+        entries = session.query(Entry)
+        entries = entries.order_by(Entry.datetime.desc())
+        entries = entries[start:end]
+        
+        return render_template("entries.html",
+            entries=entries,
+            has_next=has_next,
+            has_prev=has_prev,
+            page=page,
+            total_pages=total_pages
+        )
+    except ValueError:
+        flash(u'Try a whole number between 1 - 50!', 'danger')
+        
+        limit = PAGINATE_BY
+        page_index = page - 1
+    
+        count = session.query(Entry).count()
+    
+        start = page_index * limit
+        end = start + limit
+    
+        total_pages = (count - 1) / limit + 1
+        has_next = page_index < total_pages - 1
+        has_prev = page_index > 0
+    
+        entries = session.query(Entry)
+        entries = entries.order_by(Entry.datetime.desc())
+        entries = entries[start:end]
+        
+        return render_template("entries.html",
+            entries=entries,
+            has_next=has_next,
+            has_prev=has_prev,
+            page=page,
+            total_pages=total_pages
+        )
+    
 
 @app.route("/entry/add", methods=["GET"])
 @login_required
@@ -87,12 +120,16 @@ def edit_entry_get(id):
 @app.route("/entry/<id>/edit", methods=["POST"])
 @login_required
 def edit_entry_post(id):
-    entry = session.query(Entry).get(id)
-    entry.title = request.form["title"]
-    entry.content = request.form["content"]
-    session.commit()
-    return redirect(url_for("entries"))
-    
+    if "cancel" in request.form:
+        return redirect(url_for("entries"))
+    else:
+        entry = session.query(Entry).get(id)
+        print(request.form)
+        entry.title = request.form["title"]
+        entry.content = request.form["content"]
+        session.commit()
+        return redirect(url_for("entries"))
+        
     
 @app.route("/entry/<id>/delete", methods=["GET"])
 def delete_entry_get(id):
@@ -106,3 +143,11 @@ def delete_entry_post(id):
     session.delete(entry)
     session.commit()
     return redirect(url_for("entries"))
+    
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash(u'You are logged out', 'danger')
+    return redirect(url_for("entries"))
+    
